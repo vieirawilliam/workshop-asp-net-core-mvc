11cd6f401de4dd13953fd1a588e14ee96147f42a
William Vieira <willian_43@hotmail.com>
2019-10-15 15:11:41 -0300

    Update Seller and custom service exception

--- SalesWebMvc/Controllers/SellersController.cs
+++ SalesWebMvc/Controllers/SellersController.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 using SalesWebMvc.Models;
 using SalesWebMvc.Models.ViewModels;
 using SalesWebMvc.Services;
+using SalesWebMvc.Services.Exceptions;
 
 namespace SalesWebMvc.Controllers
 {
@@ -24,7 +25,7 @@ namespace SalesWebMvc.Controllers
 
         public IActionResult Index()
         {
-            var list = _sellerService.FindAll();        
+            var list = _sellerService.FindAll();
             return View(list);
         }
 
@@ -52,9 +53,9 @@ namespace SalesWebMvc.Controllers
                 return NotFound();
             }
 
-            var obj = _sellerService.FindById(id.Value); 
+            var obj = _sellerService.FindById(id.Value);
 
-            if(obj == null)
+            if (obj == null)
             {
                 return NotFound();
             }
@@ -70,7 +71,7 @@ namespace SalesWebMvc.Controllers
             return RedirectToAction(nameof(Index));
         }
 
-        public IActionResult Details (int? id)
+        public IActionResult Details(int? id)
         {
             if (id == null)
             {
@@ -86,5 +87,52 @@ namespace SalesWebMvc.Controllers
 
             return View(obj);
         }
+
+        public IActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var obj = _sellerService.FindById(id.Value);
+
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            List<Department> departments = _departmentService.FindAll();
+            SellerFormViewModel viewModel = new SellerFormViewModel { Seller = obj, Departments = departments };
+
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(int id, Seller seller)
+        {
+            if (id != seller.Id)
+            {
+                return BadRequest();
+            }
+
+            try
+            {
+                _sellerService.Update(seller);
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch (NotFoundException)
+            {
+                return NotFound();
+            }
+            catch (DbConcurrencyException)
+            {
+                return BadRequest();
+            }
+        }
+
     }
+
 }
